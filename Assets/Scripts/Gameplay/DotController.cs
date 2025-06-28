1f3c5e4eca5caabaf2f3e18cc9cc9df52a3de878
gplaychiencute <chiennt.gplaystudio@gmail.com>
2025-06-28 22:44:27 +0700

    up

--- Assets/Scripts/Gameplay/DotController.cs
+++ Assets/Scripts/Gameplay/DotController.cs
@@ -1,29 +1,30 @@
 ﻿using UnityEngine;
 using DG.Tweening;
+using UnityEngine.XR;
 
 namespace ChienNT
 {
     public class DotController : MonoBehaviour
     {
         [SerializeField] private Transform hand;
-        [SerializeField] float rotateDuration = 1f;
-        [SerializeField] float targetAngle;
+        [SerializeField] private float rotateSpeed = 90f;
 
         [HideInInspector]
         public Tween rotateTween;
 
-        public void RotateToAngle(float angle)
-        {
-            if (rotateTween != null && rotateTween.IsActive()) rotateTween.Kill();
 
+        public void StartRotating()
+        {
+            if (rotateTween != null && rotateTween.IsActive())
+            {
+                rotateTween.Kill();
+                rotateTween = null;
+            }
+            float currentZ = transform.eulerAngles.z;
             rotateTween = transform
-                .DORotate(new Vector3(0, 0, angle), rotateDuration)
-                .SetEase(Ease.OutCubic)
-                .OnComplete(() =>
-                {
-                    Debug.Log("Done");
-                    rotateTween = null;
-                });
+                .DORotate(new Vector3(0, 0, currentZ + 360f), 360f / rotateSpeed, RotateMode.FastBeyond360)
+                .SetEase(Ease.Linear)
+                .SetLoops(-1, LoopType.Restart);
         }
 
         public void StopRotationOnCollision(GameObject collidedDot)
@@ -32,17 +33,19 @@ namespace ChienNT
             {
                 rotateTween.Kill();
                 rotateTween = null;
-                Debug.Log($"Hand va chạm với {collidedDot.name}, đã dừng Tween.");
+                Debug.Log($"Hand va chạm với {collidedDot.name}");
             }
 
             hand.SetParent(collidedDot.transform);
             hand.gameObject.GetComponent<HandCollisionDetector>().parentDotController = collidedDot.GetComponent<DotController>();
-            collidedDot.GetComponent<CircleCollider2D>().radius = 0.5f;
         }
 
         void OnMouseDown()
         {
-            RotateToAngle(targetAngle);
+            hand.SetParent(gameObject.transform);
+            hand.gameObject.GetComponent<HandCollisionDetector>().parentDotController = gameObject.GetComponent<DotController>();
+            Debug.Log("CLICKED DOT: " + gameObject.name);
+            StartRotating();
         }
     }
-}
+} 
